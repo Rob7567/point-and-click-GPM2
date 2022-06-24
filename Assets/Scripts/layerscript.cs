e5928bdc6fdbec404aead8cb972dbcb2e2b9a6b4
Rob <robert.lange@student.hnu.de>
2022-06-24 20:50:58 +0200

    wand colider + layering

--- Assets/Scripts/layerscript.cs
+++ Assets/Scripts/layerscript.cs
@@ -5,23 +5,54 @@ using UnityEngine;
 public class layerscript : MonoBehaviour
 {
     public GameObject player;
-    public bool test;
+    public bool layer;
+    public bool invlayer;
     public SpriteRenderer spriterenderer;
     
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.layer == LayerMask.NameToLayer("layertrigger"))
         {
-            test = true;
-            spriterenderer.sortingOrder = 1;
+            layer = true;
+            
         }
+        if (other.gameObject.layer == LayerMask.NameToLayer("invlayertrigger"))
+        {
+            invlayer = true;
+            spriterenderer.sortingOrder = -1;
+        }
+
     }
         private void OnTriggerExit2D(Collider2D other)
     {
         if (other.gameObject.layer == LayerMask.NameToLayer("layertrigger"))
         {
-            test = true;
-            spriterenderer.sortingOrder = 3;
+            layer = false;
+            
+        }
+        if (other.gameObject.layer == LayerMask.NameToLayer("invlayertrigger"))
+        {
+            invlayer = false;
+            spriterenderer.sortingOrder = 1;
+        }
+    }
+    public void Update()
+    {
+        if (layer == true)
+        {
+            spriterenderer.sortingOrder = 1;
+        }
+        if (layer == false )
+        {
+            if (invlayer == true)
+            {
+                spriterenderer.sortingOrder = -1;
+            }
+            else
+            {
+                spriterenderer.sortingOrder = 3;
+            }
+            
         }
     }
 
